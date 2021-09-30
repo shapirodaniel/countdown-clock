fd47dfc8f813bd8441e97a386230b923289e7f74
shapirodaniel <shapirodanieladam@gmail.com>
2021-09-29 21:05:10 -0500

    add: functionality

--- clock.js
+++ clock.js
@@ -5,6 +5,8 @@ const CLEAR_BTN = document.getElementById("clear");
 const TIME_INPUT = document.getElementById("timeInput");
 const SET_TIME_BTN = document.getElementById("setTime");
 
+let secondInterval;
+
 const updateStartStopBtn = () => {
   const update = START_STOP_BTN.innerHTML === "START" ? "STOP" : "START";
   START_STOP_BTN.innerHTML = update;
@@ -13,13 +15,44 @@ const updateStartStopBtn = () => {
 START_STOP_BTN.addEventListener("click", () => {
   START_STOP_BTN.classList.toggle("start");
   START_STOP_BTN.classList.toggle("stop");
+
   updateStartStopBtn();
+
+  if (START_STOP_BTN.innerHTML === "STOP") {
+    secondInterval = setInterval(() => {
+      let minutes = +MINUTES.innerHTML;
+
+      if (SECONDS.innerHTML === "00") {
+        if (minutes === 0) {
+          START_STOP_BTN.classList.toggle("start");
+          START_STOP_BTN.classList.toggle("stop");
+          START_STOP_BTN.innerHTML = "START";
+          clearInterval(secondInterval);
+        } else {
+          let newMinutes = minutes - 1;
+          MINUTES.innerHTML = String(newMinutes);
+          SECONDS.innerHTML = "59";
+        }
+      } else if (+SECONDS.innerHTML === 10 || /0\d/g.test(SECONDS.innerHTML)) {
+        let seconds = +SECONDS.innerHTML;
+        seconds--;
+        SECONDS.innerHTML = "0" + seconds;
+      } else {
+        let seconds = +SECONDS.innerHTML;
+        SECONDS.innerHTML = --seconds;
+      }
+    }, 1000);
+  } else {
+    clearInterval(secondInterval);
+  }
 });
 
 CLEAR_BTN.addEventListener("click", () => {
   START_STOP_BTN.classList.remove("stop");
   START_STOP_BTN.classList.add("start");
   START_STOP_BTN.innerHTML = "START";
+  MINUTES.innerHTML = "0";
+  SECONDS.innerHTML = "00";
 });
 
 SET_TIME_BTN.addEventListener("click", () => {
